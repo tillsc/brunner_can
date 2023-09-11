86c275864f18b57f62df58ead5c7a5fcd07a657f
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2023-09-11 11:25:20 +0200

    Fix DBC format again

--- convert.rb
+++ convert.rb
@@ -1,7 +1,11 @@
 require 'csv'
+require 'set'
 
 old_device = nil
+devices = Set.new
 out_files = Hash.new { |h, k| h[k] = '' }
+sensors = ""
+sensor_cust = ""
 CSV.foreach('in.csv', col_sep: ';', headers: true) do |row|
   id = row['ID'].to_i(16)
   if id > 0
@@ -11,26 +15,27 @@ CSV.foreach('in.csv', col_sep: ';', headers: true) do |row|
     mid = "#{row['Device']}_#{row['Group']}_#{row['Module']}"
     mid <<  "_#{row['Name']}" if row['Name'].to_s != ''
     mid = mid.upcase.gsub('-', '_').gsub(/\?*/, "").gsub(/\(.*\)/, "").strip.gsub(" ", "_")
-    dbc_filename = "#{row['Device'].downcase}.dbc"
-    out_files[dbc_filename] << "BO_ #{id} #{mid}: 4 #{row['Device']}\n"
-    out_files[dbc_filename] << "  SG_ #{mid} : 15|16@0- (#{row['Scale'] || '1'},0) [0|0] \"#{row['Unit']}\" DBG\n"
-    out_files[dbc_filename] << "\n"
-    out_files[dbc_filename] << "CM_ BO_ #{id} \"#{row['Group']} #{row['Module']} #{row['Name']}\";\n"
-    out_files[dbc_filename] << "CM_ SG_ #{id} #{mid} \"#{row['Annotations']}\";\n"
-    out_files[dbc_filename] << "\n"
+    device = row['Device']
+    devices << device
+    out_files[device] << "BO_ #{id} #{mid}: 4 #{row['Device']}\n"
+    out_files[device] << "  SG_ #{mid} : 15|16@0- (#{row['Scale'] || '1'},0) [0|0] \"#{row['Unit']}\" DBG\n"
+    out_files[device] << "\n"
+    out_files["#{device}_CM"] << "CM_ BO_ #{id} \"#{row['Group']} #{row['Module']} #{row['Name']}\";\n"
+    out_files["#{device}_CM"] << "CM_ SG_ #{id} #{mid} \"#{row['Annotations']}\";\n"
+    out_files["#{device}_CM"] << "\n"
 
-    out_files['hassio.sensors.yaml'] << "- platform: mqtt\n"
-    out_files['hassio.sensors.yaml'] << "  name: '#{mid.downcase}'\n"
-    out_files['hassio.sensors.yaml'] << "  unit_of_measurement: '#{row['Unit']}'\n"
-    out_files['hassio.sensors.yaml'] << "  state_topic: \"#{mid.sub('_', '/').sub('_', '/').downcase}\"\n"
+    sensors << "- platform: mqtt\n"
+    sensors << "  name: '#{mid.downcase}'\n"
+    sensors << "  unit_of_measurement: '#{row['Unit']}'\n"
+    sensors << "  state_topic: \"#{mid.sub('_', '/').sub('_', '/').downcase}\"\n"
 
-    out_files['hassio.customize.yaml'] << "sensor.#{mid.downcase}:\n"
-    out_files['hassio.customize.yaml'] << "  friendly_name: '#{name}'\n"
+    sensor_cust << "sensor.#{mid.downcase}:\n"
+    sensor_cust << "  friendly_name: '#{name}'\n"
   end
 end
 
-out_files.each do |filename, content|
-  File.write("out/#{filename}", <<EOF)
+devices.each do |device|
+  File.write("out/#{device.downcase}.dbc", <<EOF)
 VERSION ""
 
 NS_ : 
@@ -38,8 +43,10 @@ NS_ :
 
 BS_:
 
-BU_: #{filename.gsub(".dbc", "").upcase}
+BU_: #{device}
 
-#{content}
+#{out_files[device]}
+
+#{out_files["#{device}_CM"]}
 EOF
 end
